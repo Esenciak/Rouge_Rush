8292c5c5af02cfd297b2cb68e2b906ec13607c16
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-21 21:29:50 +0100

    upd

--- Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -2,27 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RoomNodeTypeListSO", menuName = "Scriptable Object/Dungeon/Room Node Type List")]
-
+[CreateAssetMenu(fileName = "RoomNodeTypeListSO", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
 public class RoomNodeTypeListSO : ScriptableObject
 {
-	#region Header ROOM NODE TYPE LIST
-	[Space(10)]
-	[Header("ROOM NODE TYPE LIST")]
-	#endregion
-	#region Tooltip
-	[Tooltip("Tu powinny siê zgraæ RoomNodeTypeListSO, uzywane zamiast enum")]
-	#endregion
-	public List<RoomNodeTypeSO> list;
+    #region Header ROOM NODE TYPE LIST
+    [Space(10)]
+    [Header("ROOM NODE TYPE LIST")]
+    #endregion
+    #region Tooltip
+    [Tooltip("This list should be populated with all the RoomNodeTypeSO for the game - it is used instead of an enum")]
+    #endregion
+    public List<RoomNodeTypeSO> list;
 
-
-
-	#region Validation
+    #region Validation
 #if UNITY_EDITOR
-	private void OnValidate()
-	{
-		HelperUtilities.ValidateCheckENumerableValues(this, nameof(list), list);
-	}
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckENumerableValues(this, nameof(list), list);
+    }
 #endif
-	#endregion
+    #endregion
 }
