6b0c76d70d1c75bd9f2991c0172de3ff6f2f4b54
Karol Jabłoński <Karol.jablonski00@gmail.com>
2024-01-21 16:14:31 +0100

    RoomEditor
    Poprawiłem miejsce wyświetlenia room editora

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {   // menu z dokumentacji unity
-    [MenuItem("Room Node Graph Editor",menuItem = "Windows/Dungeon Editor/Room Node Graph Editor")]
+    [MenuItem("Room Node Graph Editor",menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
     // otwieranie edytora
     private static void OpenWindow()
     {
